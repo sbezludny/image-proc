ac28ffcea86fc2493c6084054b8b722f627ea92b
Serg <undrdog@yandex.ru>
2014-05-10 13:51:54 +0300

    Median filter tuning

--- dist/improc.js
+++ dist/improc.js
@@ -173,6 +173,20 @@ if (typeof define !== 'undefined' && define.amd) {
 
 	improcjs.MedianFilter = function() {
 
+		var median = function(values) {
+			values.sort(function(a, b) {
+				return a - b;
+			});
+
+			var half = Math.floor(values.length / 2);
+
+			if (values.length % 2) {
+				return values[half];
+			} else {
+				return (values[half - 1] + values[half]) / 2;
+			}
+		};
+
 		var processFilter = function(sourceBytes, rect, size) {
 
 			var w = rect.width,
@@ -180,62 +194,38 @@ if (typeof define !== 'undefined' && define.amd) {
 				x, y,
 				index,
 				rx, ry, t, r, g, b;
-			// processing square's radius
+
 			var radius = size >> 1;
-			// number of elements
+
 			var c = 0;
 
-			// array to hold pixel values (R, G, B)
+			var result = new Uint8ClampedArray(sourceBytes);
 
-
-
-			var result = new Uint8ClampedArray(w * h * 4);
-
-			for (x = 0; x < w; x++) {
-				for (y = 0; y < h; y++) {
-					index = (y * w + x) * 4;
+			for (y = 0; y < h; y++) {
+				for (x = 0; x < w; x++) {
 					c = 0;
 					r = [];
 					g = [];
 					b = [];
-					for (rx = -radius; rx <= radius; rx++) {
-						t = x + rx;
+					for (ry = y - radius; ry <= y + radius; ry++) {
+						for (rx = x - radius; rx <= x + radius; rx++) {
 
-						if (t < 0) {
-							continue;
-						}
+							var ri = (ry * w + rx) * 4;
 
-						if (t > w) {
-							break;
-						}
-
-						for (ry = -radius; ry <= radius; ry++) {
-							t = y + ry;
-							if (t < 0) {
-								continue;
-							}
-
-							if (t < h) {
-
-								var ri = ((y + ry) * w + x + rx) * 4;
-								r[c] = sourceBytes[ri];
-								g[c] = sourceBytes[ri + 1];
-								b[c] = sourceBytes[ri + 2];
-								c++;
-							} else {
-								break;
-							}
+							r[c] = sourceBytes[ri];
+							g[c] = sourceBytes[ri + 1];
+							b[c] = sourceBytes[ri + 2];
+							c++;
 						}
 					}
 
-					t = c >> 1;
-
-
-
-					result[index] = r.sort()[t];
-					result[index + 1] = g.sort()[t];
-					result[index + 2] = b.sort()[t];
-					result[index + 3] = 255;
+					index = (y * w + x) * 4;
+					result[index] = median(r);
+					result[index + 1] = median(g);
+					result[index + 2] = median(b);
+					/*result[index] = median(r);
+					result[index + 1] = median(g);
+					result[index + 2] = median(b);*/
 
 				}
 			}
